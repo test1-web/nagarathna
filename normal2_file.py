de628f563b65d7c5ccc33fefa950641d3c8952ce
chukkaumasankar <35135462+chukkaumasankar@users.noreply.github.com>
2025-01-18 07:25:05 +0530

    changes made

--- normal2_file.py
+++ normal2_file.py
@@ -2,6 +2,8 @@ def method1(a,b):
     sum1=a+b
     return sum1
 x=method1(2333 ,67)
-print(f"sumation of two numbers is is {x}")
+print(f"addition of given twn numbers is: {x}")
 print("bash is now default one ")
 print("this is a new file in nagarathna refrsctory")
+print("i suceeded in doing code change and code push to github")
+print("its my new victory")
