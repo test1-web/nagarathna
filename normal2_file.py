3e0cbc2a2a9fd667866f2c6feb335749c5194622
chukkaumasankar <35135462+chukkaumasankar@users.noreply.github.com>
2025-01-18 17:41:37 +0530

    subtraction-2

--- normal2_file.py
+++ normal2_file.py
@@ -6,9 +6,8 @@ def method2(a,b):
 x=method1(2333 ,67)
 print(f"addition of given twn numbers is: {x}")
 print("bash is now default one ")
-print("this is a new file in nagarathna refrsctory")
-print("i suceeded in doing code change and code push to github")
-print("its my new victory")
+
+
 y=method2(23,70)
 print(f"subtraction between two nums is : {y}")
 print("this will comes on subtraction branch")
