267a539d03673c8b2ea4585c018d3b3dfac633d2
chukkaumasankar <35135462+chukkaumasankar@users.noreply.github.com>
2025-01-18 07:07:38 +0530

    file change to new refroctory folder

---
+++ normal2_file.py
@@ -0,0 +1,7 @@
+def method1(a,b):
+    sum1=a+b
+    return sum1
+x=method1(2333 ,67)
+print(f"sumation of two numbers is is {x}")
+print("bash is now default one ")
+print("this is a new file in nagarathna refrsctory")
